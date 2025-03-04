7fcac3a7b8392155cc0e835878571d7ba8fe2698
Sulav Adhikari <sulavadh124@gmail.com>
2025-03-04 20:09:00 +0545

    hlo

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -4,3 +4,8 @@ import pandas as pd
 
 def distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2)**2))
+#hlo
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred)**2)
+
+#day 11
